273b052fc290c787759af42e5aa9325c9d471e02
xbl <x_bl@sina.com>
2019-12-09 14:32:08 +0800

    refactor(UserList): add service layer

--- app/src/main/java/com/xbl/noodlecodedemo/vm/UserVM.java
+++ app/src/main/java/com/xbl/noodlecodedemo/vm/UserVM.java
@@ -2,37 +2,33 @@ package com.xbl.noodlecodedemo.vm;
 
 import android.app.Application;
 import android.content.Intent;
-import android.os.Handler;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 import com.xbl.noodlecodedemo.UserListActivity;
-import com.xbl.noodlecodedemo.db.UserDBHelper;
 import com.xbl.noodlecodedemo.model.User;
+import com.xbl.noodlecodedemo.service.UserService;
 
 public class UserVM extends AndroidViewModel {
     public User user;
     private Application application;
-    private UserDBHelper helper;
+    private UserService userService;
 
 
     public UserVM(@NonNull Application application) {
         super(application);
         this.application = application;
-        helper = new UserDBHelper(application);
+        userService = new UserService(application);
         user = new User();
     }
 
     public void onInsert() {
-        new Handler().post(() -> {
-            if (helper.existByName(user.getName())) {
-                Toast.makeText(application, "用户名已经存在！", Toast.LENGTH_SHORT).show();
-                return ;
-            }
-            helper.inserUser(user);
-            Toast.makeText(application, "插入成功！", Toast.LENGTH_SHORT).show();
+        userService.insertUser(user, (object) -> {
+            Boolean isSuccess = (Boolean) object;
+            String tipsText = isSuccess ? "插入成功！": "用户名已经存在！";
+            Toast.makeText(application, tipsText, Toast.LENGTH_SHORT).show();
         });
     }
 
@@ -43,12 +39,8 @@ public class UserVM extends AndroidViewModel {
     }
 
     public void onDestroy() {
-        /*
-          保持数据库链接，数据库建立连接的开销比较大，所以尽量在 Activity 销毁之前关闭连接。
-          https://developer.android.com/training/data-storage/sqlite.html#PersistingDbConnection
-         */
-        if (helper != null) {
-            helper.close();
+        if (userService != null) {
+            userService.onDestroy();
         }
     }
 }
